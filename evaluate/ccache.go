297c4dbfc1c9f654ba0b9334e5ac4df928713c29
Ruogu Du <duruogu@pku.edu.cn>
2018-10-23 12:42:51 -0400

    test new ccache

--- evaluate/ccache.go
+++ evaluate/ccache.go
@@ -1,13 +1,25 @@
 package evaluate
 
 import (
-	"github.com/Onmysofa/ccache"
+	ccache_page "github.com/Onmysofa/ccache"
+	"github.com/karlseguin/ccache"
 	"time"
 )
 
+func EvalCcachePage(size int64, num int, itemsPruning uint32, ttl time.Duration, thread int) float64 {
+
+	var cache = ccache_page.New(ccache_page.Configure().MaxSize(size).ItemsToPrune(itemsPruning).Buckets(64))
+
+	ins := func (key, val string) {
+		cache.Set(key, val, ttl)
+	}
+
+	return insertUtil(ins, num, thread, "Ccache")
+}
+
 func EvalCcache(size int64, num int, itemsPruning uint32, ttl time.Duration, thread int) float64 {
 
-	var cache = ccache.New(ccache.Configure().MaxSize(size).ItemsToPrune(itemsPruning))
+	var cache = ccache.New(ccache.Configure().MaxSize(size).ItemsToPrune(itemsPruning).Buckets(64))
 
 	ins := func (key, val string) {
 		cache.Set(key, val, ttl)
--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/Onmysofa/pagelevelcache/parse"
+	"github.com/Onmysofa/pagelevelcache/evaluate"
+	"math"
+	"time"
 )
 
 func main() {
 
-	//for i := 4; i < 7; i++ {
-	//	for j := 4; j <= 7; j++ {
-	//		size := math.Pow10(i)
-	//		num := math.Pow10(j)
-	//		qps := evaluate.EvalCcache(int64(size), int(num), 10, time.Minute * 10, 10)
-	//		fmt.Printf("%v ", qps);
-	//	}
-	//	fmt.Println("")
-	//}
+	for i := 4; i < 7; i++ {
+		for j := 4; j <= 7; j++ {
+			size := math.Pow10(i)
+			num := math.Pow10(j)
+			qps := evaluate.EvalCcachePage(int64(size), int(num), 10, time.Minute * 10, 10)
+			fmt.Printf("%v ", qps);
+		}
+		fmt.Println("")
+	}
 
 	//for t := 1; t < 6; t++ {
 	//	qps :=evaluate.EvalCcache(1000000, 10000000, 10, time.Minute * 10, t)
@@ -40,12 +42,12 @@ func main() {
 
 	//evaluate.EvalGcache(1000, 1000000, 10)
 
-	ch, err := parse.ParseFile("/home/ruogu/Desktop/capstone/data/first1000.json")
-	if err != nil {
-		return
-	}
-
-	for req := range ch {
-		fmt.Println(req)
-	}
+	//ch, err := parse.ParseFile("/home/ruogu/Desktop/capstone/data/first1000.json")
+	//if err != nil {
+	//	return
+	//}
+	//
+	//for req := range ch {
+	//	fmt.Println(req)
+	//}
 }
