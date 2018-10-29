0a4dfdeb177b9de17186e2014529eafc18c3c6ff
Ruogu Du <duruogu@pku.edu.cn>
2018-10-29 11:01:14 -0400

    add trace

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/Onmysofa/pagelevelcache/evaluate"
+	"github.com/Onmysofa/pagelevelcache/parse"
 	"math"
 	"time"
 )
 
 func main() {
 
-	for i := 4; i < 7; i++ {
-		for j := 4; j <= 7; j++ {
-			size := math.Pow10(i)
-			num := math.Pow10(j)
-			qps := evaluate.EvalCcachePage(int64(size), int(num), 100, time.Minute * 10, 10)
-			fmt.Printf("%v ", qps);
-		}
-		fmt.Println("")
-	}
+	//for i := 4; i < 7; i++ {
+	//	for j := 4; j <= 7; j++ {
+	//		size := math.Pow10(i)
+	//		num := math.Pow10(j)
+	//		qps := evaluate.EvalCcachePage(int64(size), int(num), 100, time.Minute * 10, 10)
+	//		fmt.Printf("%v ", qps);
+	//	}
+	//	fmt.Println("")
+	//}
 
 	//for t := 1; t < 6; t++ {
 	//	qps :=evaluate.EvalCcache(1000000, 10000000, 10, time.Minute * 10, t)
@@ -42,16 +43,18 @@ func main() {
 
 	//evaluate.EvalGcache(1000, 1000000, 10)
 
-	//ch, err := parse.ParseFile("/home/ruogu/Desktop/capstone/data/first100000.json")
-	//if err != nil {
-	//	return
-	//}
-	//t := time.Now()
-	//for req := range ch {
-	//	fmt.Println(req)
-	//}
-	//d := time.Now().Sub(t)
-	//fmt.Println(100000 / d.Seconds())
+	ch, err := parse.ParseFile("/home/ruogu/Desktop/capstone/data/first100000.json")
+	if err != nil {
+		return
+	}
+	for i := 4; i < 7; i++ {
+		size := math.Pow10(i)
+		qps := evaluate.EvalCcacheTrace(ch, int64(size), 100000, 100, time.Minute * 10, 10)
+		fmt.Printf("%v ", qps);
+		fmt.Println("")
+	}
 
 	//parse.ParititionFile("/home/ruogu/Desktop/capstone/data/trace_2018_03_06_24h.json", 8)
+
+
 }
