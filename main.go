8c08c524a90d8fccd1756934d7a02a49c0bd622c
Ruogu Du <duruogu@pku.edu.cn>
2018-11-18 10:45:36 -0500

    remove comments

--- main.go
+++ main.go
@@ -10,61 +10,6 @@ import (
 
 func main() {
 
-	//for i := 4; i < 7; i++ {
-	//	for j := 4; j <= 7; j++ {
-	//		size := math.Pow10(i)
-	//		num := math.Pow10(j)
-	//		qps := evaluate.EvalCcachePage(int64(size), int(num), 100, time.Minute * 10, 10)
-	//		fmt.Printf("%v ", qps);
-	//	}
-	//	fmt.Println("")
-	//}
-
-	//for t := 1; t < 6; t++ {
-	//	qps :=evaluate.EvalCcache(1000000, 10000000, 10, time.Minute * 10, t)
-	//	fmt.Printf("%v ", qps);
-	//}
-
-
-	//for i := 4; i < 7; i++ {
-	//	for j := 4; j <= 7; j++ {
-	//		size := math.Pow10(i)
-	//		num := math.Pow10(j)
-	//		qps := evaluate.EvalGcache(int(size), int(num), 10)
-	//		fmt.Printf("%v ", qps);
-	//	}
-	//	fmt.Println("")
-	//}
-
-	//for t := 1; t < 6; t++ {
-	//	qps := evaluate.EvalGcache(1000000, 10000000, 10)
-	//	fmt.Printf("%v ", qps);
-	//}
-
-	//evaluate.EvalGcache(1000, 1000000, 10)
-
-	//ch, err := parse.ParseFile("/home/ruogu/Desktop/capstone/data/first1000.json")
-	//if err != nil {
-	//	return
-	//}
-	//for i := 7; i < 10; i++ {
-	//	size := math.Pow10(i)
-	//	qps := evaluate.EvalCcacheTrace(ch, int64(size), 1000, 100, time.Minute * 10, 10)
-	//	fmt.Printf("%v ", qps);
-	//	fmt.Println("")
-	//}
-
-	//parse.ParititionFile("/home/ruogu/Desktop/capstone/data/trace_2018_03_06_24h.json", 8)
-
-	//argsWithoutProg := os.Args[1:]
-	//size, err := strconv.ParseInt(argsWithoutProg[1], 10, 64)
-	//if err != nil {
-	//	return
-	//}
-	//threads, err := strconv.ParseInt(argsWithoutProg[2], 10, 64)
-	//funBenchTrace(argsWithoutProg[0], size, int(threads))
-	//funCalcUniqueSize(argsWithoutProg[0])
-
 	tracePtr := flag.String("t", "", "The trace to test against")
 	sizePtr := flag.Int64("s", 5000000000, "The size of cache by byte")
 	granularityPtr := flag.Int("g", 30000, "The granularity to report")
